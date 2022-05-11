a1c6a00d7408fbc5f1e0ba9665b34a24b412f104
davelange <lange@finiam.com>
2022-05-11 15:35:07 +0100

    Improve falling blocks

--- src/components/FallingBlock.svelte
+++ src/components/FallingBlock.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onDestroy, onMount } from "svelte";
+  import Shapes from "./Shapes.svelte";
 
   export let index: number;
 
@@ -7,20 +8,14 @@
   let y = 0;
   let interval;
   let randomVisible = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
-  let enterTimeout = index * 600;
-  let visible = false;
+  /* let enterTimeout = Math.floor(Math.random() * (1500 - 200 + 1)) + 200; */
+  let enterTimeout = index * 900;
 
   let colors = ["#ff5050", "#158CFA", "#F9F25D", "#D05DF9", "#5DF9DD", "#fff"];
 
-  const randomPosX = () => {
-    let minX = window.innerWidth / 5;
-
-    return Math.floor(Math.random() * (minX * 3 - minX + 1)) + minX;
-  };
-
   function initPosition() {
-    y = 100;
-    x = randomPosX();
+    x = Math.max((index * window.innerWidth) / 8, 20);
+    y = Math.floor(Math.random() * (0 - -200 + 1)) + -200;
   }
 
   function changePosition() {
@@ -35,7 +30,6 @@
     initPosition();
 
     setTimeout(() => {
-      visible = true;
       setInterval(changePosition, 900);
     }, enterTimeout);
   });
@@ -49,85 +43,28 @@
   class="wrapper"
   style:top={`${y}px`}
   style:left={`${x}px`}
-  style:opacity={visible ? 1 : 0}
   style={`--rand: ${colors[index]}`}
 >
-  <!-- large square -->
-  <svg
-    style:display={randomVisible === 1 ? "block" : "none"}
-    width="318"
-    height="271"
-    viewBox="0 0 318 271"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect width="318" height="271" />
-  </svg>
-
-  <!-- rectangle -->
-  <svg
-    style:display={randomVisible === 2 ? "block" : "none"}
-    width="318"
-    height="113"
-    viewBox="0 0 318 113"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect width="318" height="113" />
-  </svg>
-
-  <!-- L shape -->
-  <svg
-    style:display={randomVisible === 3 ? "block" : "none"}
-    width="318"
-    height="215"
-    viewBox="0 0 318 215"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect width="318" height="113" />
-    <rect
-      x="205"
-      y="215"
-      width="215"
-      height="113"
-      transform="rotate(-90 205 215)"
-    />
-  </svg>
-
-  <!-- double L shape -->
-  <svg
-    style:display={randomVisible === 4 ? "block" : "none"}
-    width="318"
-    height="317"
-    viewBox="0 0 318 317"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect y="102" width="318" height="113" />
-    <rect
-      x="205"
-      y="317"
-      width="215"
-      height="113"
-      transform="rotate(-90 205 317)"
-    />
-    <rect y="215" width="215" height="113" transform="rotate(-90 0 215)" />
-  </svg>
+  <Shapes shape={randomVisible} />
 </div>
 
 <style>
   .wrapper {
     position: absolute;
-    width: 100px;
+    width: 80px;
     opacity: 0.7;
     filter: drop-shadow(3px 3px 0 #111);
   }
-  svg {
-    max-width: 100%;
-    max-height: 100%;
+
+  @media (min-width: 500px) {
+    .wrapper {
+      width: 100px;
+    }
   }
-  rect {
-    fill: var(--rand);
+  
+  @media (min-width: 1440px) {
+    .wrapper {
+      width: 150px;
+    }
   }
 </style>
---
+++ src/components/Shapes.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  export let shape: number;
+
+  console.log(shape);
+</script>
+
+{#if shape === 1}
+  <!-- large square -->
+  <svg
+    width="318"
+    height="271"
+    viewBox="0 0 318 271"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="318" height="271" />
+  </svg>
+{:else if shape === 2}
+  <!-- rectangle -->
+  <svg
+    width="318"
+    height="113"
+    viewBox="0 0 318 113"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="318" height="113" />
+  </svg>
+{:else if shape === 3}
+  <!-- L shape -->
+  <svg
+    width="318"
+    height="215"
+    viewBox="0 0 318 215"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="318" height="113" />
+    <rect
+      x="205"
+      y="215"
+      width="215"
+      height="113"
+      transform="rotate(-90 205 215)"
+    />
+  </svg>
+{:else if shape === 4}
+  <!-- double L shape -->
+  <svg
+    width="318"
+    height="317"
+    viewBox="0 0 318 317"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="102" width="318" height="113" />
+    <rect
+      x="205"
+      y="317"
+      width="215"
+      height="113"
+      transform="rotate(-90 205 317)"
+    />
+    <rect y="215" width="215" height="113" transform="rotate(-90 0 215)" />
+  </svg>
+{/if}
+
+<style>
+  svg {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  rect {
+    fill: var(--rand);
+  }
+</style>
